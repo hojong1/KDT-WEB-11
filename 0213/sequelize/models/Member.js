f4f76b334ce4c24a7fdecb9eb3009648dbd60ffa
hojong1 <ksa9132@naver.com>
2024-02-13 14:00:45 +0900

    0213수업

---
+++ 0213/sequelize/models/Member.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+
+const MemberModel = (sequelize) => {
+    const Member = sequelize.define(
+        'member',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false, // NOT NULL
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            userid: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            username: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+        },
+        {
+            tablename: 'member',
+            freezeTableName: true,
+            timestamps: false,
+        }
+    );
+    return Member;
+};
+
+module.exports = MemberModel;
