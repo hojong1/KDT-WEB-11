2bb17f3dac20a4aac73015f31ff83b0ca49db893
hojong1 <ksa9132@naver.com>
2024-02-14 13:55:08 +0900

    0214 수업

---
+++ 0214/squelize2/controller/index.js
@@ -0,0 +1,16 @@
+const main = (req, res) => {
+    res.render('index');
+};
+
+const post = (req, res) => {
+    res.render('post'); //전체 글
+};
+
+const detail = (req, res) => {
+    res.render('detail'); //상세 페이지
+};
+const write = (req, res) => {
+    res.render('write'); //글쓰기 페이지
+};
+
+module.exports = { main, post, detail, write };
