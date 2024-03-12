9513ee2214d43aa83a6bdb52b0b436c875fd951e
hojong1 <ksa9132@naver.com>
2024-03-12 13:56:57 +0900

    0312 수업

---
+++ 0312/0312-props/src/FunctionComponent.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+export default function FunctionComponent({ name, time, children }) {
+  //   const { name } = props; // 이걸 넣은 순간 props.name > name
+
+  //파라미터에도 props 대신 {name}을 넣고 name으로 사용할 수 있다.
+  return (
+    <div>
+      {/* <h1>Hi {props.name}</h1> */}
+      <h1>Hi {name}</h1>
+      {/* <h1>Hi {props.time}</h1> */}
+      <h1>Hi {time}</h1>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+FunctionComponent.defaultProps = {
+  name: '홍길동',
+};
+FunctionComponent.propTypes = {
+  name: PropTypes.string,
+};
